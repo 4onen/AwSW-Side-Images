42032cb2e58b76f2ce06d7bd1c108fc0905a8457
4onen <mattjdupree@gmail.com>
2021-09-27 16:23:40 -0700

    Add exit code to confirmation script.

--- confirm.sh
+++ confirm.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+EXIT_CODE=0
+
 for IMAGE_PATH in $(./list.awk side_images.rpy.pre | grep "^[^#]")
 do
     if ! test -f ../../$IMAGE_PATH -o -f ../**/resource/$IMAGE_PATH
     then
+        EXIT_CODE=1
         echo "$IMAGE_PATH does not exist!"
     fi
 done
+
+exit $EXIT_CODE
